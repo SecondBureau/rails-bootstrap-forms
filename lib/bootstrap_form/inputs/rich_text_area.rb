8904545c0c7c02042ccafe0580e0c982b80421e0
Gilles Crofils <gilles@secondbureau.com>
2020-04-04 21:54:26 +0200

    Monkey Patching for Rails 4
    Designed for our own needs.
    ID is not standard.

--- lib/bootstrap_form/inputs/rich_text_area.rb
+++ lib/bootstrap_form/inputs/rich_text_area.rb
@@ -7,16 +7,18 @@ module BootstrapForm
       include Base
 
       included do
-        def rich_text_area_with_bootstrap(name, options={})
+        def rich_text_area(name, options={})
           form_group_builder(name, options) do
             prepend_and_append_input(name, options) do
               options[:class] = ["trix-content", options[:class]].compact.join(" ")
-              rich_text_area_without_bootstrap(name, options)
+              id = "trix_editor_#{SecureRandom.hex(10)}"
+              options[:id] = id
+              hidden_field(name, options) + content_tag('trix-editor', "", input: id)
             end
           end
         end
 
-        bootstrap_alias :rich_text_area
+        # bootstrap_alias :rich_text_area
       end
     end
   end
